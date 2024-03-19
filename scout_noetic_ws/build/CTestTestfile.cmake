47c41cb5b446dd40ce20f6182e73e999d5921de6
Neil Patel <patelneil8152@gmail.com>
2024-03-19 12:44:05 -0400

    added noetic_ws and started building roller_eye

---
+++ scout_noetic_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/herolab/Desktop/moorebot-scout/scout_noetic_ws/src
+# Build directory: /home/herolab/Desktop/moorebot-scout/scout_noetic_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("roller_eye")
---
+++ scout_noetic_ws/build/roller_eye/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/herolab/Desktop/moorebot-scout/scout_noetic_ws/src/roller_eye
+# Build directory: /home/herolab/Desktop/moorebot-scout/scout_noetic_ws/build/roller_eye
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
