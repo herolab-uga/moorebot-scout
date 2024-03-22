ee57f7934592fd4405814839f44da08968e7c6e3
slemsz <slems.s760@gmail.com>
2024-03-21 22:06:38 -0400

    altered ./shell/lib/sense/pipe/logger.sh to refect changes made in write.sh

--- shell/lib/sense/pipe/logger.sh
+++ shell/lib/sense/pipe/logger.sh
@@ -1,7 +1,17 @@
 #!/bin/bash 
 
 # Log data from /tmp/imu to the specified port 
-cat /tmp/imu_pipe | nc -l -p 8030 & 
+cat /tmp/imu_data | nc -l -p 8030 & 
 
 # Log data from /tmp/odom to the specified port 
-cat /tmp/odom_pipe | nc -l -p 8031 & 
+cat /tmp/odom_data | nc -l -p 8031 & 
+
+
+cat /tmp/tof_data
+
+cat /tmp/simple_battery_status_data
+
+cat /tmp/h264
+
+cat /tmp/light
+
