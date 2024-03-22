ed99ef049d67550a3d15a5316550dd51661e8912
slemsz <slems.s760@gmail.com>
2024-03-21 22:02:44 -0400

    changed write.sh to add support for battery_status, tof, raw camera data, and illuminance data.

--- shell/lib/sense/pipe/write.sh
+++ shell/lib/sense/pipe/write.sh
@@ -6,8 +6,12 @@
 init() { 
     echo "init called"
     # set pipe paths 
-    IMU_PIPE="/tmp/imu_pipe"
-    ODOM_PIPE="/tmp/odom_pipe"
+    IMU_DIR="/tmp/imu_data"
+    ODOM_DIR="/tmp/baselink_odom_relative_data"
+    TOF_DIR="/tmp/tof_data"
+    BAT_DIR="/tmp/simple_battery_status_data"
+    LIGHT_DIR="/tmp/light"
+    IMG_DIR="/tmp/h264"
 }
 
 # make pipe 
@@ -18,12 +22,24 @@ make_pipe() {
         mkfifo $PIPE_DIR || { echo "failed to create named pipe at location: $PIPE_DIR"; exit 1; }
     fi
     case "$PIPE_DIR" in 
-        "$IMU_PIPE") 
-            echo "IMU pipe created"
-            rostopic echo /SensorNode/imu > $IMU_PIPE;;
-        "$ODOM_PIPE") 
-            echo "ODOM pipe created"
-            rostopic echo /MotorNode/baselink_odom_relative > $ODOM_PIPE;;
+        "$IMU_DIR") 
+            echo "imu pipe created at $IMU_DIR"
+            rostopic echo /SensorNode/imu > $IMU_DIR;;
+        "$ODOM_DIR") 
+            echo "odom pipe created at $ODOM_DIR"
+            rostopic echo /MotorNode/baselink_odom_relative > $ODOM_DIR;;
+        "$TOF_DIR")
+            echo "tof pipe created at $TOF_DIR"
+            rostopic echo /SensorNode/tof > $TOF_DIR;;
+        "$BAT_DIR")
+            echo "simple battery status pipe created at $BAT_DIR";;
+            rostopic echo /SensorNode/simple_battery_status > $BAT_DIR;;
+        "$LIGHT_DIR")
+            echo "illuminance pipe created at $LIGHT_DIR";;
+            rostopic echo /SensorNode/light
+        "$IMG_DIR")
+            echo "img pipe created at $IMG_DIR"
+            rostopic echo /CoreNode/h264 > $IMG_DIR;;
         *) 
             echo "unknown err";;
     esac
@@ -32,8 +48,15 @@ make_pipe() {
 # start
 start() { 
     echo "start called"
-    make_pipe $IMU_PIPE & 
-    make_pipe $ODOM_PIPE &
+    make_pipe $IMU_DIR & 
+    make_pipe $ODOM_DIR &
+    # needs to be tested 
+    ## 
+    # make_pipe $TOF_DIR & 
+    # make_pipe $BAT_DIR & 
+    # make_pipe $LIGHT_DIR & 
+    # make_pipe $IMG_DIR & 
+    ## end  
     echo "^+C to quit." 
     wait
 }
